9313e0aeadc5eda5f7036dfb80bcb6671be56aba
Dane Vanderbilt <danevandy@hotmail.com>
2021-02-02 17:31:09 -0600

    Got basic login and signup process working

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,3 +1,4 @@
+import { AuthService } from './../../shared/service/auth.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  isNavbarCollapsed: boolean = true;
+
+  constructor(
+    public authService: AuthService
+  ) { }
 
   ngOnInit(): void {
   }
--- src/app/shared/service/auth.service.ts
+++ src/app/shared/service/auth.service.ts
@@ -3,12 +3,14 @@ import { User } from "../models/user";
 import { AngularFireAuth } from "@angular/fire/auth";
 import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/firestore';
 import { Router } from "@angular/router";
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   userData: any; // Save logged in user data
+  isLoggedIn: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(JSON.parse(localStorage.getItem('user')) !== null);
 
   constructor(
     public store: AngularFirestore,   // Inject Firestore service
@@ -21,21 +23,23 @@ export class AuthService {
     this.auth.authState.subscribe(user => {
       if (user) {
         this.userData = user;
+        this.isLoggedIn.next(true);
         localStorage.setItem('user', JSON.stringify(this.userData));
         JSON.parse(localStorage.getItem('user'));
       } else {
         localStorage.setItem('user', null);
+        this.isLoggedIn.next(false);
         JSON.parse(localStorage.getItem('user'));
       }
     })
   }
 
   // Sign in with email/password
-  signIn(email, password) {
+  logIn(email, password) {
     return this.auth.signInWithEmailAndPassword(email, password)
       .then((result) => {
         this.ngZone.run(() => {
-          this.router.navigate(['dashboard']);
+          this.router.navigate(['home']);
         });
         this.setUserData(result.user);
       }).catch((error) => {
@@ -63,12 +67,6 @@ export class AuthService {
     }).catch((error) => {
       window.alert(error)
     })
-  }
-
-  // Returns true when user is looged in and email is verified
-  get isLoggedIn(): boolean {
-    const user = JSON.parse(localStorage.getItem('user'));
-    return user !== null;
   }
 
   // Auth logic to run auth providers
@@ -101,8 +99,8 @@ export class AuthService {
     })
   }
 
-  // Sign out
-  signOut() {
+  // Logout
+  logout() {
     return this.auth.signOut().then(() => {
       localStorage.removeItem('user');
       this.router.navigate(['log-in']);
