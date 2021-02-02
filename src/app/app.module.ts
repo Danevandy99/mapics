5b500759a24595e578249c172ea4cb91d454ea69
Dane Vanderbilt <danevandy@hotmail.com>
2021-02-02 15:02:38 -0600

    WIP

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+
+const config = {
+  apiKey: "AIzaSyBiBcN3svwTQk_jOdfVz239d0zZX4FKthE",
+  authDomain: "mapics-b0365.firebaseapp.com",
+  projectId: "mapics-b0365",
+  storageBucket: "mapics-b0365.appspot.com",
+  messagingSenderId: "775450242286",
+  appId: "1:775450242286:web:0418b435adb40f2cc1d7ff",
+  measurementId: "G-BBFNGTJ5QD"
+};
 
 @NgModule({
   declarations: [
@@ -10,7 +24,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(config),
+    AngularFirestoreModule, // firestore
+    AngularFireAuthModule, // auth
+    AngularFireStorageModule // storage
   ],
   providers: [],
   bootstrap: [AppComponent]
