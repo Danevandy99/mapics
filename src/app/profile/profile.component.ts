6eead19ad93396fd551b2a735d3eedf5ca8aa6e4
Dane Vanderbilt <danevandy@hotmail.com>
2021-02-04 16:04:57 -0600

    Added profile UI

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,11 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+export enum ProfileTabState {
+  HIGHLIGHTS = 0,
+  GRID = 1,
+  VERTICAL = 2
+};
+
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent implements OnInit {
+
+  profileTabState: ProfileTabState = ProfileTabState.HIGHLIGHTS;
 
   constructor() { }
 
