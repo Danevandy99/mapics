ebff1f20fec0b020a77c8792f867d9d185556823
Dane Vanderbilt <danevandy@hotmail.com>
2021-02-25 15:58:45 -0600

    Fix cache bug

--- src/app/profile/follow-popup/follow-popup.component.ts
+++ src/app/profile/follow-popup/follow-popup.component.ts
@@ -1,7 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { ActivatedRoute } from '@angular/router';
-import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
+import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
 import { filter, map, switchMap } from 'rxjs/operators';
 import { UserSettings } from 'src/app/shared/models/user';
 import { AuthService } from '../../shared/service/auth.service';
@@ -22,45 +22,48 @@ export class FollowPopupComponent implements OnInit {
   @Input() usersType: string;
   users: Array<UserSettings> = [];
 
-  ngOnInit(): void {    
-    this.route.params.subscribe(params => {
-      this.getUsers();
-    })
-    console.log(this.userId);
+  ngOnInit(): void { }
+
+  ngOnChanges(): void {
+    this.getUsers();
   }
-  ngOnDestroy(): void{
+
+  ngOnDestroy(): void {
     this.modalService.dismissAll();
   }
+
+
   closeResult = '';
 
-  getUsers(){
-      this.store.collection('users').doc(this.userId).collection(this.usersType)
+  getUsers() {
+    this.users = [];
+    this.store.collection('users').doc(this.userId).collection(this.usersType)
       .get()
       .pipe(
         map(docs => {
-          return docs.docs.map( doc => {
+          return docs.docs.map(doc => {
             return { ...<Object>doc.data(), userId: doc.id } as UserSettings
           })
         })
       )
       .subscribe(users => {
         users.forEach(user => {
-        this.store.collection('users').doc(user.userId)
-        .get()
-        .pipe( map
-          (doc => {
-            return { ...<object>doc.data(), userId: doc.id } as UserSettings;
-          })
-        )
-        .subscribe(userSetting => {
-          this.users.push(userSetting);
-        })
-      });
+          this.store.collection('users').doc(user.userId)
+            .get()
+            .pipe(map
+              (doc => {
+                return { ...<object>doc.data(), userId: doc.id } as UserSettings;
+              })
+            )
+            .subscribe(userSetting => {
+              this.users.push(userSetting);
+            })
+        });
       });
   }
 
   open(content) {
-    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+    this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title' }).result.then((result) => {
       this.closeResult = `Closed with: ${result}`;
     }, (reason) => {
       this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
